88c9e1b521824c4893e98815c597fee3ce5b4bc9
Stephen Ferguson <sferg989@gmail.com>
2025-07-22 23:11:20 -0500

    seems to work

--- packages/astro-utils/index.ts
+++ packages/astro-utils/index.ts
@@ -1,4 +1,7 @@
 // index.ts
 export { default as MyComponent } from './src/MyComponent.astro';
 export { default as VideoDisplay } from './src/components/VideoDisplay.astro';
+export { default as Footer } from './src/components/Footer.astro';
+export { default as FormattedDate } from './src/components/FormattedDate.astro';
+export { default as SocialLinks } from './src/components/SocialLinks.astro';
 export * from './src/util/module';
