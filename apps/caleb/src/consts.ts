8942bacab0acc0983bd2367b1f2919d77b50a721
Stephen Ferguson <sferg989@gmail.com>
2025-07-22 23:22:27 -0500

    Hmm

--- apps/caleb/src/consts.ts
+++ apps/caleb/src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Caleb Ferguson';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Caleb Ferguson 🦈';
+export const SITE_DESCRIPTION = 'Apex predator of the digital ocean. Navigating through orange sunsets and deep waters.';
 export const SITE_URL = 'https://caleb-ferguson.com';
